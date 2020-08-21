ddbef59514c61472e246990f9df9fa26c5548fd0
Franck Binard <franckbinard@gmail.com>
2020-08-20 21:42:42 -0400

    added ability to erase board

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Cell from './Cell'; 
 import './Grid.css';
- 
+
+
+
+const gridInfo = (rows,cols)=>Array(rows).fill(null).map((x,i)=>Array(cols).fill(i).map((i,j)=>""))
+
 class Grid extends React.Component {
 
   constructor(props) {
@@ -11,29 +15,46 @@ class Grid extends React.Component {
          cols:30,
          rows:20
       }
-
-      this.state.gridInfo = Array(this.state.rows).fill(null)
-      .map((x,i)=>Array(this.state.cols).fill(i).map((i,j)=>""))
-  }
+      this.state.gridInfo =  gridInfo(this.state.rows, this.state.cols)
+   } 
 
   val(i,j){ return this.state.gridInfo[i][j] }
-
+   
   setVal(i,j) {
       let gridInfo = [...this.state.gridInfo]
       gridInfo[i][j] = gridInfo[i][j]==='X'?"":'X' 
       this.setState({gridInfo})
   } 
+   resetGrid(){
+      this.setState({gridInfo:gridInfo(this.state.rows, this.state.cols)})
+   }
 
-  row(numElements,i){
+   row(numElements,i){
       return Array(numElements).fill(null)
       .map((x,j)=><button className="cell" onClick={()=>this.setVal(i,j)}>{this.val(i,j)}</button>)
    }
 
-   renderRow(i){return (<div className="board-row">{this.row(this.state.cols,i)}</div>)}
+   renderRow(i){
+      return (<div className="board-row">{this.row(this.state.cols,i)}</div>)
+   }
 
-   render() { return Array(this.state.rows).fill(null).map((_,i)=>{
+   renderGrid() { 
+      return Array(this.state.rows).fill(null).map((_,i)=>{
          return (<div>{this.renderRow(i)}</div>); 
       })
+   }
+
+   render(){
+      return (
+            <div className="container">
+            <div>
+                {this.renderGrid()}
+            </div>
+            <div>
+               <button onClick={()=>this.resetGrid()}>replay</button>
+            </div>
+            </div>
+      )
    }
 }
 
