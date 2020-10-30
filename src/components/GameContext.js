4475be8b0043d089c8ed987c85367115d493c5a3
Franck Binard <franckbinard@gmail.com>
2020-10-29 23:40:12 -0400

    some css changes

--- src/components/GameContext.js
+++ src/components/GameContext.js
@@ -3,16 +3,77 @@ import React, {useState, createContext} from 'react';
 
 export const GameContext = createContext(); 
 
-export const GameProvider = props => {
-    const [dotLocation, setDotLocation] = useState([
+
+const cacheInfoHandler = async function(){
+    const checkCache = 'caches' in window
+    if(checkCache === null) throw "cache api not available" 
+    return caches.open('mineGrinder') 
+}
+
+const readPlayerInfo = async function( result, playerInfoReadableStream ){
+    // read() returns a promise that resolves
+    // when a value has been received
+    return new Promise((resolve, reject)=> {
+        return playerInfoReadableStream.read()
+        .then(function processText({ done, value }) {
+      // Result objects contain two properties:
+      // done  - true if the stream has already given you all its data.
+      // value - some data. Always undefined when done is true.
+            if (done) {
+                console.log("Stream complete");
+                return resolve(result);
+            } else {
+                result.push(value)
+                return resolve( readPlayerInfo(result, playerInfoReadableStream))
+            }
+        })
+    })
+}
+
+export class GameProvider extends React.Component {
+
+    constructor( props )   {
+        super(props);
+        this.state = {
+            playerName: 'anonymous'
+        } 
+        /*const [dotLocation, setDotLocation] = useState([
         {
             i:11, 
             j:15
         }
-    ]); 
-    return (
-        <GameContext.Provider value={'fdsa'}>
-            {props.children}
-        </GameContext.Provider>
-    ); 
+        ]); */
+    } 
+
+    componentDidMount(){
+
+        let gameCache = null; 
+        return cacheInfoHandler()
+        .then( gc => {
+            gameCache = gc
+            return gameCache.match('PLAYER_NAME')
+        })
+        .then ( player => {
+            if(player === undefined){ //No stored player name, create
+                return gameCache.put('/PLAYER_INFO', new Response(
+                    '{"name": "Player 1"}'
+                ));
+            } else {
+                let reader = player.body.getReader(); 
+                return readPlayerInfo([], reader )
+            }
+        })
+        .then( readerText => {
+            debugger
+            return 1
+        })
+    }
+
+    render() {
+        return (
+            <GameContext.Provider value={'fdsa'}>
+                {this.props.children}
+            </GameContext.Provider>
+        );
+    } 
 }
